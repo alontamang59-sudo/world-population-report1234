e0654489046556eea5d43649b14a1dd53dba9625
alontamang59-sudo <alontamang59@gmail.com>
2026-06-02 13:06:52 +0000

    add country and city classes

---
+++ src/Country.java
@@ -0,0 +1,8 @@
+public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    private int population;
+    private String capital;
+}
---
+++ src/city.java
@@ -0,0 +1,6 @@
+public class city {
+    private String name;
+    private String country;
+    private String district;
+    private int population;
+}
