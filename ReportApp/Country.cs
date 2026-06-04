898e23d5959cda2a8fda99963c58e6198f65ea1a
alontamang59-sudo <alontamang59@gmail.com>
2026-06-04 06:52:46 +0000

    Work in progress

--- ReportApp/Country.cs
+++ ReportApp/Country.cs
@@ -1,8 +1,8 @@
 public class Country
 {
-    public string? Code { get; set; }
-    public string? Name { get; set; }
-    public string? Continent { get; set; }
-    public string? Region { get; set; }
+    public string Code { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Continent { get; set; } = "";
+    public string Region { get; set; } = "";
     public int Population { get; set; }
 }
